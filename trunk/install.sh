521a8c87f4fffe3b707c7f2f09d7f96c7895cc34
oops <oops@65dd6fbe-65ce-11e0-a205-d8f6118655d0>
2004-08-04 14:54:04 +0000

    update 3.2.0

--- trunk/install.sh
+++ trunk/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # version numbering
-VER="3.1.2"
+VER="3.2.0"
 
 # Root user의 권한으로 작동시켜야 함
 #
@@ -14,18 +14,24 @@ if [ "$UID" != "0" ]; then
   exit 1
 fi
 
-ETCDIR="/etc/oops_firewall"
-DOCDIR="/usr/doc/oops_firewall-${VER}"
+ETCDIR="/etc/oops-firewall"
+DOCDIR="/usr/doc/oops-firewall-${VER}"
 
+[ -f "/sbin/oops-firewall" ] && rm -f /sbin/oops-firewall
 [ -f "/sbin/oops_firewall" ] && rm -f /sbin/oops_firewall
 
 mkdir ${ETCDIR}
-install -m 700 ./oops-firewall /usr/sbin/oops_firewall
+if [ -d "/etc/oops_firewall" ]; then
+  mv -f /etc/oops_fireall /etc/oops_firewall/backup
+fi
+
+install -m 700 ./oops-firewall-${VER} /usr/sbin/oops-firewall
 install -m 644 ./filter.conf ${ETCDIR}/filter.conf
 install -m 644 ./masq.conf ${ETCDIR}/masq.conf
 install -m 644 ./forward.conf ${ETCDIR}/forward.conf
-install -m 644 ./client.conf ${ETCDIR}/client.conf
 install -m 644 ./string.conf ${ETCDIR}/string.conf
+install -m 644 ./tos.conf ${ETCDIR}/tos.conf
+install -m 644 ./user.conf ${ETCDIR}/user.conf
 
 mkdir -p ${DOCDIR}
 install -m 644 ./COPYING ${DOCDIR}/COPYING
@@ -35,17 +41,19 @@ install -m 644 ./Changelog ${DOCDIR}/Changelog
 install -m 644 ./INSTALL ${DOCDIR}/INSTALL
 install -m 644 ./README ${DOCDIR}/README
 install -m 644 ./README.SORIBADA ${DOCDIR}/README.SORIBADA
-install -m 644 ./README.SORIBADA ${DOCDIR}/README.STRING
+install -m 644 ./README.STRING ${DOCDIR}/README.STRING
+install -m 644 ./README.DIALPAD ${DOCDIR}/README.DIALPAD
 install -m 755 ./uninstall.sh ${DOCDIR}/uninstall.sh
-install -m 755 ./oops-firewall.init ${DOCDIR}/oops_firewall.init
+install -m 755 ./oops-firewall.init ${DOCDIR}/oops-firewall.init
 
 echo "mkdir ${ETCDIR}" >> ${DOCDIR}/install.log
-echo "install -m 700 ./oops-firewall /usr/sbin/oops_firewall" >> ${DOCDIR}/install.log
+echo "install -m 700 ./oops-firewall-${VER} /usr/sbin/oops-firewall" >> ${DOCDIR}/install.log
 echo "install -m 644 ./filter.conf ${ETCDIR}/filter.conf" >> ${DOCDIR}/install.log
 echo "install -m 644 ./masq.conf ${ETCDIR}/masq.conf" >> ${DOCDIR}/install.log
 echo "install -m 644 ./forward.conf ${ETCDIR}/forward.conf" >> ${DOCDIR}/install.log
-echo "install -m 644 ./client.conf ${ETCDIR}/client.conf" >> ${DOCDIR}/install.log
 echo "install -m 644 ./string.conf ${ETCDIR}/string.conf" >> ${DOCDIR}/install.log
+echo "install -m 644 ./tos.conf ${ETCDIR}/tos.conf" >> ${DOCDIR}/install.log
+echo "install -m 644 ./user.conf ${ETCDIR}/user.conf" >> ${DOCDIR}/install.log
 echo "" >> ${DOCDIR}/install.log
 echo "mkdir -p ${DOCDIR}" >> ${DOCDIR}/install.log
 echo "install -m 644 ./COPYING ${DOCDIR}/COPYING" >> ${DOCDIR}/install.log
@@ -55,7 +63,8 @@ echo "install -m 644 ./Changelog ${DOCDIR}/Changelog" >> ${DOCDIR}/install.log
 echo "install -m 644 ./INSTALL ${DOCDIR}/INSTALL" >> ${DOCDIR}/install.log
 echo "install -m 644 ./README ${DOCDIR}/README" >> ${DOCDIR}/install.log
 echo "install -m 644 ./README.SORIBADA ${DOCDIR}/README.SORIBADA" >> ${DOCDIR}/install.log
+echo "install -m 644 ./README.DIALPAD ${DOCDIR}/README.DIALPAD" >> ${DOCDIR}/install.log
 echo "install -m 755 ./uninstall.sh ${DOCDIR}/uninstall.sh" >> ${DOCDIR}/install.log
-echo "install -m 755 ./oops-firewall.init ${DOCDIR}/oops_firewall.init" >> ${DOCDIR}/install.log
+echo "install -m 755 ./oops-firewall.init ${DOCDIR}/oops-firewall.init" >> ${DOCDIR}/install.log
 
 exit 0
