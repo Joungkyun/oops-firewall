4547521de27922d1ede1740916286912f278fbe9
nobody <nobody@65dd6fbe-65ce-11e0-a205-d8f6118655d0>
2004-08-04 14:43:46 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_0_0'.

---
+++ tags/v3_0_0/uninstall.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# version numbering
+VER="3.0"
+
+# Root user의 권한으로 작동시켜야 함
+#
+if [ "$UID" != "0" ]; then
+  if [ "$LANG" = "ko" ]; then
+    echo "에러 - Root 의 권한으로 작동해야 합니다."
+  else
+    echo "U are not super user!"
+  fi
+  exit 1
+fi
+
+/sbin/iptables -Z
+/sbin/iptables -P INPUT ACCEPT
+/sbin/iptables -F
+/sbin/iptables -t nat -Z
+/sbin/iptables -t nat -P PREROUTING ACCEPT
+/sbin/iptables -t nat -F 
+/sbin/iptables -t nat -P POSTROUTING ACCEPT
+/sbin/iptables -t nat -F 
+/sbin/iptables -t mangle -Z 
+/sbin/iptables -t mangle -P PREROUTING ACCEPT
+/sbin/iptables -t mangle -F PREROUTING
+/sbin/iptables -t mangle -P OUTPUT ACCEPT
+/sbin/iptables -t mangle -F OUTPUT
+
+rm -rf /sbin/oops_firewall
+rm -rf /usr/doc/oops_firewall-${VER}
+
+exit 0
