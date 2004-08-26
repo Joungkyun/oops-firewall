c3faeddc9c5dc2099bc413d347cda20ef2394ae9
oops <oops@65dd6fbe-65ce-11e0-a205-d8f6118655d0>
2004-08-26 13:31:30 +0000

    fixed to use ppp device for masq function

--- trunk/oops-firewall.h
+++ trunk/oops-firewall.h
@@ -1,6 +1,6 @@
 # OOPS Firewall 에서 사용되는 함수
 #
-# $Id: oops-firewall.h,v 1.8 2004-08-18 11:05:26 oops Exp $
+# $Id: oops-firewall.h,v 1.9 2004-08-26 13:31:30 oops Exp $
 #
 # 사용자 실행을 위한 함수
 user_cmd () {
@@ -218,5 +218,247 @@ kernelCheck() {
     return 0
   else
     return 1
+  fi
+}
+
+# 네트워크 디바이스 (eth/ppp) 목록을 얻어오는 함수
+# getDeviceList 목록변수명 체크디바이스이름
+#
+# 목록 변수명은 디바이스 리스트를 가질 변수 이름을 지정
+# 체크 디바이스이름을 지정할 경우 해당 디바이스가 존재하면 0을 리턴
+# 존재하지 않으면 1을 리턴함으로서 디바이스 존재여부를 체크할 수 있음
+#
+getDeviceList() {
+  devVarName=$1
+  devCheckVar=$2
+  devGetVar=$(${grep} '\(eth\|ppp\)[0-9]\+:' /proc/net/dev | ${awk} -F ':' '{print $1}')
+
+  if [ -z "${devGetVar}" ]; then
+    return 1
+  else
+    if [ -n "${devVarName}" ]; then
+      devTmpVarName="${devVarName}=\"${devGetVar}\""
+      eval ${devTmpVarName}
+    fi
+  fi
+
+  if [ -n "${devCheckVar}" ]; then
+    devCheckVarOK=$(echo ${devGetVar} | ${grep} "${devCheckVar}")
+    if [ -n "${devCheckVarOK}" ]; then
+      return 0
+    else
+      return 1
+    fi
+  fi
+
+  return 0
+}
+
+makeDeviceEnv() {
+  devEnvDevName=$1
+
+  devEnvVar_NAME=
+  devEnvVar_NUMBER=
+  parseDevice "${devEnvDevName}" devEnvVar_NAME devEnvVar_NUMBER
+
+  getDeviceIP "${devEnvDevName}" devEnvVar_IP
+  getDeviceMask "${devEnvDevName}" devEnvVar_MASK
+  getDeviceNetwork "${devEnvVar_IP}" "${devEnvVar_MASK}" devEnvVar_NW
+
+  devTmpVar="${devEnvVar_NAME}${devEnvVar_NUMBER}"
+  devTmpIP="${devTmpVar}_IPADDR=${devEnvVar_IP}"
+  devTmpSM="${devTmpVar}_SUBNET=${devEnvVar_MASK}"
+  devTmpNW="${devTmpVar}_NET=${devEnvVar_NW}"
+  eval ${devTmpIP}
+  eval ${devTmpSM}
+  eval ${devTmpNW}
+}
+
+parseDevice() {
+  parseDevDeviceName=$1
+  parseDevName=$2
+  parseDevNum=$3
+
+  parseTmpName=$(echo ${parseDevDeviceName} | ${sed} 's/[0-9]\+//g')
+  parseTmpNum=$(echo ${parseDevDeviceName} | ${sed} 's/eth\|ppp//g')
+
+  WordToUpper ${parseTmpName} parseTmpName
+
+  eval "${parseDevName}=\"${parseTmpName}\""
+  eval "${parseDevNum}=\"${parseTmpNum}\""
+}
+
+getDeviceIP() {
+  getDeviceIPDevName=$1
+  getDeviceIPVarName=$2
+
+  getDeviceIPTmp=$(${SIFCONFIG} ${getDeviceIPDevName} 2> /dev/null | \
+                   ${grep} 'inet addr' | \
+                   ${awk} '{print $2}' | \
+                   ${sed} -e 's/.*://g')
+
+  if [ -n "${getDeviceIPVarName}" ]; then
+    getDeviceIPTmpVar="${getDeviceIPVarName}=\"${getDeviceIPTmp}\""
+    eval ${getDeviceIPTmpVar}
+  fi
+}
+
+getDeviceMask() {
+  getDeviceMaskDevName=$1
+  getDeviceMaskVarName=$2
+
+  getDeviceMaskTmp=$(${SIFCONFIG} ${getDeviceMaskDevName} 2> /dev/null | \
+                   ${grep} 'inet addr' | \
+                   ${awk} '{print $4}' | \
+                   ${sed} -e 's/.*://g')
+
+  if [ -n "${getDeviceMaskVarName}" ]; then
+    getDeviceMaskTmpVar="${getDeviceMaskVarName}=\"${getDeviceMaskTmp}\""
+    eval ${getDeviceMaskTmpVar}
+  fi
+}
+
+getDeviceNetwork() {
+  getDeviceNwDevIP=$1
+  getDeviceNwDevMask=$2
+  getDeviceNwVarName=$3
+
+  getDeviceNwTmp=$(${ipcalc} -s -n ${getDeviceNwDevIP} \
+                                   ${getDeviceNwDevMask} | \
+                   ${awk} -F '=' '{print $2}')
+
+  if [ -n "${getDeviceNwVarName}" ]; then
+    getDeviceNwTmpVar="${getDeviceNwVarName}=\"${getDeviceNwTmp}\""
+    eval ${getDeviceNwTmpVar}
+  fi
+}
+
+WordToUpper() {
+  ORGTEXT=$1
+  ORGVAR=$2
+
+  toU=
+  ORGTMP=$(echo $ORGTEXT | sed 's/\(.\)/\1 /g')
+
+  for i in $ORGTMP
+  do
+    convVar=
+    CharToUpper ${i} convVar
+    toU="${toU}${convVar}"
+  done
+
+  if [ -n "${ORGVAR}" ]; then
+    eval "${ORGVAR}=\"${toU}\""
+  fi
+}
+
+WordToLower() {
+  ORGTEXT=$1
+  ORGVAR=$2
+
+  toL=
+  ORGTMP=$(echo $ORGTEXT | sed 's/\(.\)/\1 /g')
+
+  for i in $ORGTMP
+  do
+    convVar=
+    CharToLower ${i} convVar
+    toL="${toL}${convVar}"
+  done
+
+  if [ -n "${ORGVAR}" ]; then
+    eval "${ORGVAR}=\"${toL}\""
+  fi
+}
+
+CharToUpper() {
+  ORGCHAR=$1
+  UPPERVAR=$2
+
+  case $ORGCHAR in
+    a) RETURNCHAR=A ;;
+    b) RETURNCHAR=B ;;
+    c) RETURNCHAR=C ;;
+    d) RETURNCHAR=D ;;
+    e) RETURNCHAR=E ;;
+    f) RETURNCHAR=F ;;
+    g) RETURNCHAR=G ;;
+    h) RETURNCHAR=H ;;
+    i) RETURNCHAR=I ;;
+    j) RETURNCHAR=J ;;
+    k) RETURNCHAR=K ;;
+    l) RETURNCHAR=L ;;
+    m) RETURNCHAR=M ;;
+    n) RETURNCHAR=N ;;
+    o) RETURNCHAR=O ;;
+    p) RETURNCHAR=P ;;
+    q) RETURNCHAR=Q ;;
+    r) RETURNCHAR=R ;;
+    s) RETURNCHAR=S ;;
+    t) RETURNCHAR=T ;;
+    u) RETURNCHAR=U ;;
+    v) RETURNCHAR=V ;;
+    w) RETURNCHAR=W ;;
+    x) RETURNCHAR=X ;;
+    y) RETURNCHAR=Y ;;
+    z) RETURNCHAR=Z ;;
+    *) RETURNCHAR=$ORGCHAR ;;
+  esac
+
+  if [ -n "$UPPERVAR" ]; then
+    eval "${UPPERVAR}=\"${RETURNCHAR}\""
+  fi
+}
+
+CharToLower() {
+  ORGCHAR=$1
+  LOWERVAR=$2
+
+  case $ORGCHAR in
+    A) RETURNCHAR=a ;;
+    B) RETURNCHAR=b ;;
+    C) RETURNCHAR=c ;;
+    D) RETURNCHAR=d ;;
+    E) RETURNCHAR=e ;;
+    F) RETURNCHAR=f ;;
+    G) RETURNCHAR=g ;;
+    H) RETURNCHAR=h ;;
+    I) RETURNCHAR=i ;;
+    J) RETURNCHAR=j ;;
+    K) RETURNCHAR=k ;;
+    L) RETURNCHAR=l ;;
+    M) RETURNCHAR=m ;;
+    N) RETURNCHAR=n ;;
+    O) RETURNCHAR=o ;;
+    P) RETURNCHAR=p ;;
+    Q) RETURNCHAR=q ;;
+    R) RETURNCHAR=r ;;
+    S) RETURNCHAR=s ;;
+    T) RETURNCHAR=t ;;
+    U) RETURNCHAR=u ;;
+    V) RETURNCHAR=v ;;
+    W) RETURNCHAR=w ;;
+    X) RETURNCHAR=x ;;
+    Y) RETURNCHAR=y ;;
+    Z) RETURNCHAR=z ;;
+    *) RETURNCHAR=$ORGCHAR ;;
+  esac
+
+  if [ -n "$LOWERVAR" ]; then
+    eval "${LOWERVAR}=\"${RETURNCHAR}\""
+  fi
+}
+
+masqStartCheck() {
+  SCHKTMP=$1
+  SCHKTMPNAME=$2
+
+  SCHKTMP1=$(echo ${SCHKTMP} | ${awk} -F ':' '{print $1}')
+  if [ "${SCHKTMP1}" = "0" ]; then
+    if [ -z "${SCHKTMPNAME}" ]; then
+      SCHKTMPNAME=1
+    else
+      eval "${SCHKTMPNAME}=1"
+    fi
   fi
 }
