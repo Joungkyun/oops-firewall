51fc96096e3b2eb278c16ac9787c12f44b1af0fd
nobody <nobody@65dd6fbe-65ce-11e0-a205-d8f6118655d0>
2007-04-10 11:37:36 +0000

    This commit was manufactured by cvs2svn to create tag 'v6_1_3'.

---
+++ tags/v6_1_3/src/include/oops-firewall.h
@@ -0,0 +1,20 @@
+# header include file
+#
+# $Id: oops-firewall.h,v 1.3 2007-03-29 07:55:12 oops Exp $
+#
+
+BRIDGE_NAME="brg0"
+
+source ${_includes}/color.h
+source ${_includes}/print.h
+source ${_includes}/check.h
+source ${_includes}/command.h
+source ${_includes}/default.h
+source ${_includes}/device.h
+source ${_includes}/modules.h
+source ${_includes}/init.h
+source ${_includes}/rule.h
+source ${_includes}/masq.h
+source ${_includes}/forward.h
+source ${_includes}/bridge.h
+
