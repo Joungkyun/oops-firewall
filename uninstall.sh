7365b86e71b8e033d468cf516853bd5e4bbc3d7a
JoungKyun Kim <joungkyun@daum.net>
2011-04-27 07:45:48 +0000

    rename tag name

---
+++ uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Root user의 권한으로 작동시켜야 함
+#
+if [ "$UID" != "0" ]; then
+  if [ "$LANG" = "ko" ]; then
+    echo "에러 - Root 의 권한으로 작동해야 합니다."
+  else
+    echo "U are not super user!"
+  fi
+  exit 1
+fi
+
+rm -f /etc/oops_firewall/oops_firewall.conf
+rm -rf /sbin/oops_firewall
+
+exit 0
