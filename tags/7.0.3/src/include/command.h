b3f8ce5b2b84507974e4abf5171d6f1960b826f6
oops <oops@65dd6fbe-65ce-11e0-a205-d8f6118655d0>
2017-07-29 08:38:47 +0000

    release 7.0.3

---
+++ tags/7.0.3/src/include/command.h
@@ -0,0 +1,531 @@
+# Command line variables
+#
+# $Id$
+#
+
+# command line command
+c_depmod="@depmod@"
+c_lsmod="@lsmod@"
+c_rmmod="@rmmod@"
+c_modprobe="@modprobe@"
+c_sed="@sed@"
+c_grep="@grep@"
+c_awk="@awk@"
+c_cat="@cat@"
+c_uname="@uname@"
+c_ipcalc="@ipcalc@"
+c_cut="@cut@"
+c_getopt="@getopt@"
+c_sort="@sort@"
+
+c_iptables="@iptables@"
+c_ifconfig="@ifconfig@"
+
+# bridge command
+c_route="@route@"
+
+export c_depmod c_lsmod c_rmmod c_modprobe
+export c_sed c_grep c_awk c_cat c_uname c_ipcalc c_cut
+export c_iptables c_ifconfig c_route c_sort
+
+brute_force_set() {
+	local value
+
+	[ -n "${BRUTE_FORCE_FILTER}" ] && \
+		o_echo $"  * Brute Force Attack Filter"
+
+	for value in ${BRUTE_FORCE_FILTER}
+	do
+		echo "${value}" | {
+			IFS=':' read b_port b_sec b_hit
+
+			o_echo "    iptables -A INPUT -p tcp --dport ${b_port} -m state --state NEW \\"
+			o_echo "             -m recent --set --name OFIRE_${b_port}"
+			[ ${_testmode} -eq 0 ] && \
+				${c_iptables} -A INPUT -p tcp --dport ${b_port} -m state --state NEW \
+							-m recent --set --name OFIRE_${b_port}
+			if [ $BRUTE_FORCE_LOG -eq 1 ]; then
+				o_echo "    iptables -A INPUT -p tcp --dport ${b_port} -m state --state NEW \\"
+				o_echo "             -m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \\"
+				o_echo "             --name OFIRE_${b_port} -j LOG --log-prefix SSH_Scan:"
+				[ ${_testmode} -eq 0 ] && \
+					${c_iptables} -A INPUT -p tcp --dport ${b_port} -m state --state NEW \
+								-m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \
+								--name OFIRE_${b_port} -j LOG --log-prefix SSH_Scan:
+			fi
+			o_echo "    iptables -A INPUT -p tcp --dport ${b_port} -m state --state NEW \\"
+			o_echo "             -m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \\"
+			o_echo "             --name OFIRE_${b_port} -j DROP"
+			[ ${_testmode} -eq 0 ] && \
+				${c_iptables} -A INPUT -p tcp --dport ${b_port} -m state --state NEW \
+							-m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \
+							--name OFIRE_${b_port} -j DROP
+		}
+		o_echo
+	done
+
+	#
+	# BRIDGE
+	#
+	[ $BRIDGE_USED -eq 0 ] && return
+
+	[ -n "${BRUTE_FORCE_FILTER}" ] && \
+		o_echo $"  * Bridge Brute Force Attack Filter"
+
+	for value in ${BR_BRUTE_FORCE_FILTER}
+	do
+		echo "${value}" | {
+			IFS='|' read src dest portinfo
+
+			iprange_set "${src}" src
+			iprange_check "${src}"
+			range_chk1=$?
+
+			iprange_set "${dest}" dest
+			iprange_check "${dest}"
+			range_chk2=$?
+
+			[ ${range_chk1} -eq 1 ] && srcrule="--src-range ${src}" || srcrule="-s ${src}"
+			[ ${range_chk2} -eq 1 ] && dstrule="--dst-range ${dst}" || dstrule="-d ${dest}"
+			[ ${range_chk1} -eq 1 -o ${range_chk2} -eq 1 ] && rangemod=" -m iprange"
+
+			echo "${portinfo}" | {
+				IFS=':' read b_port b_sec b_hit
+
+				o_echo "    iptables -A FORWARD -p tcp${rangemod} \\"
+				o_echo "             ${srcrule} ${dstrule} \\"
+				o_echo "             --dport ${b_port} -m state --state NEW \\"
+				o_echo "             -m recent --set --name OFIRE_${b_port}"
+				[ ${_testmode} -eq 0 ] && \
+					${c_iptables} -A FORWARD -p tcp${rangemod} \
+								${srcrule} ${dstrule} \
+								--dport ${b_port} -m state --state NEW \
+								-m recent --set --name OFIRE_${b_port}
+				if [ $BRUTE_FORCE_LOG -eq 1 ]; then
+					o_echo "    iptables -A FORWARD -p tcp${rangemod} \\"
+					o_echo "             ${srcrule} ${dstrule} \\"
+					o_echo "             --dport ${b_port} -m state --state NEW \\"
+					o_echo "             -m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \\"
+					o_echo "             --name OFIRE_${b_port} -j LOG --log-prefix SSH_Scan:"
+					[ ${_testmode} -eq 0 ] && \
+						${c_iptables} -A FORWARD -p tcp${rangemod} \
+									${srcrule} ${dstrule} \
+					   				--dport ${b_port} -m state --state NEW \
+									-m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \
+									--name OFIRE_${b_port} -j LOG --log-prefix SSH_Scan:
+				fi
+				o_echo "    iptables -A FORWARD -p tcp${rangemod} \\"
+				o_echo "             ${srcrule} ${dstrule} \\"
+				o_echo "             --dport ${b_port} -m state --state NEW \\"
+				o_echo "             -m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \\"
+				o_echo "             --name OFIRE_${b_port} -j DROP"
+				[ ${_testmode} -eq 0 ] && \
+					${c_iptables} -A FORWARD -p tcp${rangemod} \
+								${srcrule} ${dstrule} \
+								--dport ${b_port} -m state --state NEW \
+								-m recent --update --seconds ${b_sec} --hitcount ${b_hit} --rttl \
+								--name OFIRE_${b_port} -j DROP
+			}
+		}
+		o_echo
+	done
+}
+
+layer7_set() {
+	[ -n "${LAYER7_FILTER}" ] && \
+		o_echo $"  * LAYER7 Filter"
+
+	for value in ${LAYER7_FILTER}
+	do
+		echo "${value}" | {
+			IFS=':' read l_table l_chain l_proto l_action l_mark l_dir
+
+			# TABLE checking
+			#
+			case "${l_table}" in
+				"nat")    ;;
+				"mangle") ;;
+				*)
+					if [ -n "${l_table}" ]; then
+						o_echo $"     ${value} => ${l_table} is unknown table"
+						continue
+					fi
+			esac
+
+			[ -n "${l_table}" ] && pr_table="-t ${l_table} " || pr_table=
+
+			# Chain Checking
+			#
+			if [ -z "${l_chain}" ]; then
+				o_echo $"     ${value} => CHAIN value must need"
+				continue;
+			fi
+
+			case "${l_table}" in
+				"nat")
+					case "${l_chain}" in
+						"PREROUTING")  ;;
+						"POSTROUTING") ;;
+						"OUTPUT")      ;;
+						*)
+							o_echo $"     ${value} => ${l_chain} can't use with ${l_table}"
+							continue
+					esac
+					;;
+				"mangle")
+					case "${l_chain}" in
+						"PREROUTING")  ;;
+						"POSTROUTING") ;;
+						"FORWARD")     ;;
+						"OUTPUT")      ;;
+						"INPUT")       ;;
+						*)
+							o_echo $"     ${value} => ${l_chain} can't use with ${l_table}"
+							continue
+					esac
+					;;
+				*)
+					case "${l_chain}" in
+						"OUTPUT")  ;;
+						"INPUT")   ;;
+						"FORWARD") ;;
+						*)
+							o_echo $"     ${value} => ${l_chain} can't use with ${l_table}"
+							continue
+					esac
+			esac
+
+			# PROTO Checking
+			#
+			if [ -z "${l_proto}" ]; then
+				o_echo $"     ${value} => PROTO must be setting"
+				continue
+			fi
+
+			# Action Checking
+			#
+			[ -z "${l_action}" ] && l_action="DROP"
+
+			case "${l_action}" in
+				"DROP")   ;;
+				"MARK")   ;;
+				*)
+					o_echo $"     ${value} => action value is used with DROP/MARK"
+					continue;
+			esac
+
+			# Mark check
+			#
+			case "${l_action}" in
+				"MARK")
+					if [ -z "${l_mark}" ]; then
+						o_echo $"     ${value} => action MARK is needed mark value"
+						continue
+					fi
+					;;
+				"DROP")
+					[ -n "${l_mark}" ] && l_mark=
+					;;
+			esac
+
+			[ -n "${l_mark}" ] && pr_mark=" --set-mark ${l_mark}" || pr_mark=
+			[ -n "${l_dir}" ] && pr_dir="--l7dir ${l_dir} " || pr_dir=
+
+
+				o_echo "    iptables ${pr_table}-A ${l_chain} -m layer7 --l7proto ${l_proto} \\"
+				o_echo "             ${pr_dir}-j ${l_action}${pr_mark}"
+				[ ${_testmode} -eq 0 ] && \
+					${c_iptables} ${pr_table}-A ${l_chain} -m layer7 --l7proto ${l_proto} \
+								${pr_dir}-j ${l_action}${pr_mark}
+
+		}
+		o_echo
+	done
+
+	#
+	# BRIDGE
+	#
+	[ $BRIDGE_USED -eq 0 ] && return
+
+	[ -n "${BR_LAYER7_FILTER}" ] && \
+		o_echo $"  * Bridge LAYER7 Filter"
+
+	for value in ${BR_LAYER7_FILTER}
+	do
+
+		echo "${value}" | {
+			IFS='|' read src dest layerinfo
+
+			iprange_set "${src}" src
+			iprange_check "${src}"
+			range_chk1=$?
+
+			iprange_set "${dest}" dest
+			iprange_check "${dest}"
+			range_chk2=$?
+
+			[ ${range_chk1} -eq 1 ] && srcrule="--src-range ${src}" || srcrule="-s ${src}"
+			[ ${range_chk2} -eq 1 ] && dstrule="--dst-range ${dst}" || dstrule="-d ${dest}"
+			[ ${range_chk1} -eq 1 -o ${range_chk2} -eq 1 ] && rangemod=" -m iprange" || rangemod=
+
+			echo "${layerinfo}" | {
+				IFS=':' read l_table l_chain l_proto l_action l_mark l_dir
+
+				# TABLE checking
+				#
+				case "${l_table}" in
+					"nat")    ;;
+					"mangle") ;;
+					*)
+						if [ -n "${l_table}" ]; then
+							o_echo $"     ${value} => ${l_table} is unknown table"
+							continue
+						fi
+				esac
+
+				[ -n "${l_table}" ] && pr_table="-t ${l_table} " || pr_table=
+
+				# Chain Checking
+				#
+				if [ -z "${l_chain}" ]; then
+					o_echo $"     ${value} => CHAIN value must need"
+					continue;
+				fi
+
+				case "${l_table}" in
+					"nat")
+						case "${l_chain}" in
+							"PREROUTING")  ;;
+							"POSTROUTING") ;;
+							"OUTPUT")      ;;
+							*)
+								o_echo $"     ${value} => ${l_chain} can't use with ${l_table}"
+								continue
+						esac
+						;;
+					"mangle")
+						case "${l_chain}" in
+							"PREROUTING")  ;;
+							"POSTROUTING") ;;
+							"FORWARD")     ;;
+							"OUTPUT")      ;;
+							"INPUT")       ;;
+							*)
+								o_echo $"     ${value} => ${l_chain} can't use with ${l_table}"
+								continue
+						esac
+						;;
+					*)
+						case "${l_chain}" in
+							"OUTPUT")  ;;
+							"INPUT")   ;;
+							"FORWARD") ;;
+							*)
+								o_echo $"     ${value} => ${l_chain} can't use with ${l_table}"
+								continue
+						esac
+				esac
+
+				# PROTO Checking
+				#
+				if [ -z "${l_proto}" ]; then
+					o_echo $"     ${value} => PROTO must be setting"
+					continue
+				fi
+
+				# Action Checking
+				#
+				[ -z "${l_action}" ] && l_action="DROP"
+
+				case "${l_action}" in
+					"DROP")   ;;
+					"MARK")   ;;
+					*)
+						o_echo $"     ${value} => action value is used with DROP/MARK"
+						continue;
+				esac
+
+				# Mark check
+				#
+				case "${l_action}" in
+					"MARK")
+						if [ -z "${l_mark}" ]; then
+							o_echo $"     ${value} => action MARK is needed mark value"
+							continue
+						fi
+						;;
+					"DROP")
+						[ -n "${l_mark}" ] && l_mark=
+						;;
+				esac
+
+				[ -n "${l_mark}" ] && pr_mark=" --set-mark ${l_mark}" || pr_mark=
+				[ -n "${l_dir}" ] && pr_dir="--l7dir ${l_dir} " || pr_dir=
+
+
+					o_echo "    iptables ${pr_table}-A ${l_chain}${rangemod} \\"
+					o_echo "             ${srcrule} ${dstrule} \\"
+					o_echo "             -m layer7 --l7proto ${l_proto} \\"
+					o_echo "             ${pr_dir}-j ${l_action}${pr_mark}"
+					[ ${_testmode} -eq 0 ] && \
+						${c_iptables} ${pr_table}-A ${l_chain}${rangemod} \
+									${srcrule} ${dstrule} \
+									-m layer7 --l7proto ${l_proto} \
+									${pr_dir}-j ${l_action}${pr_mark}
+
+			}
+		}
+		o_echo
+	done
+}
+
+# 사용자 실행을 위한 함수
+user_cmd () {
+	case "$1" in
+		pre) 
+			brute_force_set
+			layer7_set
+			USERCHK=$(LANG="C" ${c_sed} -n -f ${_includes}/user_pre.sed ${_confdir}/user.conf)
+			IFS='%'
+			;;
+		post)
+			USERCHK=$(LANG="C" ${c_sed} -n -f ${_includes}/user_post.sed ${_confdir}/user.conf)
+			IFS='@'
+			;;
+	esac
+		
+	if [ -n "${USERCHK}" ]; then
+		for uvalue in ${USERCHK}
+		do 
+			IFS=' '
+			if [ -z "${uvalue}" ]; then
+				continue;
+			fi
+			uvalue=$(echo ${uvalue})
+			o_echo "  * ${c_iptables} ${uvalue}"
+			[ $_testmode -eq 0 ] && ${c_iptables} ${uvalue} || true
+		done
+	else 
+		IFS=' '
+		case "$1" in
+			pre)
+				o_echo $"  * USER PRE COMMAND (%) Not Config"
+				;;
+			post)
+				o_echo $"  * USER POST COMMAND (@) Not Config"
+				;;
+			*)
+				o_echo $"  * Unknown parameter" > /dev/stderr
+		esac
+	fi
+}
+
+anywhere_set() {
+	local var=$1
+	local varname=$2
+
+	[ "${var}" != "0/0" -a "${var}" != "anywhere" -a "${var}" != "ANYWHERE" ] && return
+	[ -z "${varname}" ] && return
+
+	eval "${varname}=\"0.0.0.0/0\""
+}
+
+# Usage:
+#        10.10.10.10-20          => 10.10.10.10-10.10.10.20
+#        10.10.10.10-15.20       => 10.10.10.10-10.10.15.20
+#        10.10.10.10-11.15.20    => 10.10.10.10-10.11.15.20
+#        10.10.10.10-11.11.15.20 => 10.10.10.10-11.11.15.20
+#
+iprange_set() {
+	local value=$1
+	local varname=$2
+
+	if [ $_iprange -eq 0 ]; then
+		anywhere_set "${value}" value
+		[ -n "${varname}" ] && eval "${varname}=\"${value}\""
+
+		#iprange_check $value
+		#chk=$?
+		#
+		#if [ $chk -eq 1 ]; exit 1
+		#   o_echo " !!!!!! IP set ${value}, but This kernel don't support iprange extension"
+		#   exit 1
+		#fi
+		return
+	fi
+
+	primary=${value%%-*}
+	secondary=${value##*-}
+
+	anywhere_set "${primary}" primary
+	anywhere_set "${secondary}" secondary
+
+	[ -n "${varname}" ] && unset $varname
+
+	if [ "${primary}" == "${secondary}" ]; then
+		[ -n "${varname}" ] && eval "${varname}=\"${primary}\""
+		return
+	fi
+
+	dot=$(echo "${secondary}" | ${c_sed} 's/[^.]//g')
+
+	case "$dot" in
+		"...")
+			prefix=
+			;;
+		"..")
+			prefix=$(echo ${primary} | ${c_sed} 's/[0-9]\+\.[0-9]\+\.[0-9]\+$//g')
+			;;
+		".")
+			prefix=$(echo ${primary} | ${c_sed} 's/[0-9]\+\.[0-9]\+$//g')
+			;;
+		*)
+			prefix=$(echo ${primary} | ${c_sed} 's/[0-9]\+$//g')
+	esac
+
+	secondary="${prefix}${secondary}"
+
+	if [ -n "${varname}" ]; then
+		eval "${varname}=\"${primary}-${secondary}\""
+		export ${varname}
+	fi
+}
+
+port_set() {
+	local mode=$1
+	local val=$2
+	local var=$3
+	local cvar=$4
+
+	local _port
+	local _conn
+	local constate="-m state --state"
+
+	_port=${val%%:*}
+	_conn=${val##*:}
+
+	[ "${_port}" = "${_conn}" ] && _conn=
+
+	_port=$(echo "${_port}" | ${c_sed} -e 's/-/:/g')
+
+	if [ -n "${var}" ]; then
+		[ "${mode}" = "" ] && \
+			eval "${var}=\"${_port}\"" || \
+			eval "${var}=\" --${mode}port ${_port}\""
+		export $var
+	fi
+
+	if [ -n "${cvar}" -a -n "${_conn}" ]; then
+		eval "${cvar}=\"-m state --state ${_conn}\""
+		export $cvar
+	fi
+}
+
+#
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# End:
+# vim: set filetype=sh noet sw=4 ts=4 fdm=marker:
+# vim<600: noet sw=4 ts=4:
+#
---
+++ tags/7.0.3/src/include/device.h
@@ -0,0 +1,219 @@
+# Device function
+#
+# $Id$
+#
+
+# 네트워크 디바이스 (eth/ppp/bridge) 목록을 얻어오는 함수
+# getDeviceList 목록변수명 체크디바이스이름
+#
+# 목록 변수명은 디바이스 리스트를 가질 변수 이름을 지정
+# 체크 디바이스이름을 지정할 경우 해당 디바이스가 존재하면 0을 리턴
+# 존재하지 않으면 1을 리턴함으로서 디바이스 존재여부를 체크할 수 있음
+#
+getDeviceList() {
+	devVarName=$1
+	devCheckVar=$2
+
+	devGetVar=$(${c_awk} -F ':' "/(eth|ppp|${BRIDGE_DEVNAME}|bond)[0-9]*:/ {print \$1}" /proc/net/dev | ${c_sort})
+
+	if [ -z "${devGetVar}" ]; then
+		return 1
+	else
+		if [ -n "${devVarName}" ]; then
+			devTmpVarName="${devVarName}=\"${devGetVar}\""
+			eval ${devTmpVarName}
+		fi
+	fi
+
+	if [ -n "${devCheckVar}" ]; then
+		devCheckVarOK=$(echo ${devGetVar} | ${c_grep} "${devCheckVar}")
+		if [ -n "${devCheckVarOK}" ]; then
+			return 0
+		else
+			return 1
+		fi
+	fi
+
+	return 0
+}
+
+getDeviceCheck() {
+	for i in $*
+	do
+		gDC=0
+		for j in ${devlist}
+		do
+			if [ "${i}" = "${j}" ]; then
+				gDC=1
+				break
+			fi
+		done
+
+		[ $gDC -eq 1 ] && return 0
+	done
+
+	return 1
+}
+
+makeDeviceEnv() {
+	devEnvDevName=$1
+
+	devEnvVar_NAME=
+	devEnvVar_NUMBER=
+	parseDevice "${devEnvDevName}" devEnvVar_NAME devEnvVar_NUMBER
+
+	getDeviceIP "${devEnvDevName}" devEnvVar_IP
+	getDeviceMask "${devEnvDevName}" devEnvVar_MASK
+	getDeviceNetwork "${devEnvVar_IP}" "${devEnvVar_MASK}" devEnvVar_NW
+	getDevicePrefix "${devEnvVar_IP}" "${devEnvVar_MASK}" devEnvVar_PF
+
+	devTmpVar="${devEnvVar_NAME}${devEnvVar_NUMBER}"
+	devTmpIP="${devTmpVar}_IPADDR=${devEnvVar_IP}"
+	devTmpSM="${devTmpVar}_SUBNET=${devEnvVar_MASK}"
+	devTmpNW="${devTmpVar}_NET=${devEnvVar_NW}"
+	devTmpPF="${devTmpVar}_PREFIX=${devEnvVar_PF}"
+	devTmpPP="${devTmpVar}_NETPX=\"${devEnvVar_NW}/${devEnvVar_PF}\""
+	eval ${devTmpIP}
+	eval ${devTmpSM}
+	eval ${devTmpNW}
+	eval ${devTmpPF}
+	eval ${devTmpPP}
+}
+
+parseDevice() {
+	parseDevDeviceName=$1
+	parseDevName=$2
+	parseDevNum=$3
+
+	parseTmpName=$(echo ${parseDevDeviceName} | ${c_sed} 's/[0-9]\+//g')
+	parseTmpNum=$(echo ${parseDevDeviceName} | ${c_sed} 's/eth\|ppp\|bond\|br\|tun\|tap//g')
+
+	if [ "$parseTmpName" = "$parseTmpNum" ]; then
+		eval "${parseDevNum}=\"\""
+	else
+		eval "${parseDevNum}=\"${parseTmpNum}\""
+	fi
+
+	WordToUpper ${parseTmpName} parseTmpName
+	eval "${parseDevName}=\"${parseTmpName}\""
+}
+
+getDeviceIP() {
+	getDeviceIPDevName=$1
+	getDeviceIPVarName=$2
+
+	getDeviceIPTmp=$(${c_ifconfig} ${getDeviceIPDevName} 2> /dev/null | \
+					${c_awk} '/inet (addr:)?[0-9]+\./ {print $2}' | \
+					${c_sed} 's/[^:]\+://g')
+
+	if [ -n "${getDeviceIPVarName}" ]; then
+		getDeviceIPTmpVar="${getDeviceIPVarName}=\"${getDeviceIPTmp}\""
+		eval ${getDeviceIPTmpVar}
+	fi
+}
+
+getDeviceMask() {
+	getDeviceMaskDevName=$1
+	getDeviceMaskVarName=$2
+
+	getDeviceMaskTmp=$(${c_ifconfig} ${getDeviceMaskDevName} 2> /dev/null | \
+					${c_awk} '/inet (addr:)?[0-9]+\./ {print $4}' | \
+					${c_sed} 's/[^:]\+://g')
+
+	if [ -n "${getDeviceMaskVarName}" ]; then
+		getDeviceMaskTmpVar="${getDeviceMaskVarName}=\"${getDeviceMaskTmp}\""
+		eval ${getDeviceMaskTmpVar}
+	fi
+}
+
+getDevicePrefix() {
+	getDevicePfDevIP=$1
+	getDevicePfVarMask=$2
+	getDevicePfVarName=$3
+
+	[ -z "$getDevicePfDevIP" -o -z "$getDevicePfVarMask" ] && return
+
+	if [ "${distribution}" = "debian" ]; then
+		getDevicePfTmp=$(${c_ipcalc} -n -b \
+						${getDevicePfDevIP}/${getDevicePfVarMask} | \
+						${c_grep} 'Netmask:' | \
+						${c_awk} '{print $4}' 2> /dev/null)
+	else
+		getDevicePfTmp=$(${c_ipcalc} -p ${getDevicePfDevIP} \
+						${getDevicePfVarMask} 2> /dev/null | \
+						${c_awk} -F '=' '{print $2}')
+	fi
+
+	if [ -n "${getDevicePfVarName}" ]; then
+		getDevicePfTmpVar="${getDevicePfVarName}=\"${getDevicePfTmp}\""
+		eval ${getDevicePfTmpVar}
+	fi
+}
+
+getDeviceNetwork() {
+	getDeviceNwDevIP=$1
+	getDeviceNwDevMask=$2
+	getDeviceNwVarName=$3
+
+	if [ "${distribution}" = "debian" ]; then
+		getDeviceNwTmp=$(${c_ipcalc} -n -b \
+					${getDeviceNwDevIP}/${getDeviceNwDevMask} | \
+					${c_grep} 'Network:' | \
+					${c_awk} '{print $2}' | ${c_sed} 's!/.*!!g')
+	else
+		getDeviceNwTmp=$(${c_ipcalc} -s -n \
+					${getDeviceNwDevIP} \
+					${getDeviceNwDevMask} | \
+					${c_awk} -F '=' '{print $2}')
+	fi
+
+	if [ -n "${getDeviceNwVarName}" ]; then
+		getDeviceNwTmpVar="${getDeviceNwVarName}=\"${getDeviceNwTmp}\""
+		eval ${getDeviceNwTmpVar}
+	fi
+}
+
+initInterfaceList() {
+	intName=$1
+	shift
+	intList=$*
+	TSTR=
+
+	for i in $intList
+	do
+		WordToUpper $i TMPSTR
+		TSTR="${TSTR} ${TMPSTR}"
+	done
+	TSTR=$(echo ${TSTR} | ${c_sed} 's/^[ ]\+\|[ ]\+$//g')
+
+	eval "$intName=\"${TSTR}\""
+}
+
+firewall_wan_check() {
+	[ -n "${FIREWALL_WAN}" ] && return
+
+	_donelist=
+	for i in BRIDGE_DEVNAME MASQ_DEVICE FORWARD_MASTER
+	do
+		eval "chkinter=\$${i}"
+		[ -z "${chkinter}" ] && continue
+		[ -n "$(echo ${_donelist} | ${c_grep} "-${chkinter}-")" ] && continue
+
+		FILEWALL_WAN="${FIREWALL_WAN} ${chkinter}"
+		_donelist="${_donelist} -${chkinter}-"
+	done
+
+	[ -z "${FIREWALL_WAN}" ] && FIREWALL_WAN="eth0"
+	FIREWALL_WAN=$(echo ${FIREWALL_WAN} | ${c_sed} 's/^[ ]\+\|[ ]\+$//g')
+
+	export FIREWALL_WAN
+}
+
+#
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# End:
+# vim: set filetype=sh noet sw=4 ts=4 fdm=marker:
+# vim<600: noet sw=4 ts=4:
+#
